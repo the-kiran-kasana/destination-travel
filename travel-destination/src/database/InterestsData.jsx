4774aef4c9ceeb2b40838bbb4fb7d3d8ff044ffe
kkasana <kkasanacoder@gmail.com>
2025-08-21 23:55:03 +0530

    storing the data into the db file

---
+++ travel-destination/src/database/InterestsData.jsx
@@ -0,0 +1,75 @@
+
+import { Mountain, Umbrella, Landmark } from "lucide-react";
+
+
+ export const interestsData = [
+        { id: "1",  title: "Adventure", image:"https://i.pinimg.com/1200x/d4/f3/d6/d4f3d6a6573761b8553414fe4d6f38a8.jpg" , subtitle: "Hiking, rafting, thrill rides",icon: Mountain },
+        { id: "16", title: "Adventure",  image: "https://i.pinimg.com/1200x/0c/ff/39/0cff398544f51111370511b36bf117eb.jpg" ,subtitle: "Enjoy luxury style landmarks",icon: HandPlatter},
+        { id: "18", title: "Adventure",  image: "https://i.pinimg.com/736x/aa/9b/28/aa9b2858d258261741e3d55284f550c6.jpg" ,subtitle: "Enjoy luxury style landmarks",icon: HandPlatter},
+        { id: "19", title: "Adventure",  image: "https://i.pinimg.com/1200x/91/1f/4d/911f4d44cff2a44ffe3c206589fa738f.jpg" ,subtitle: "Enjoy luxury style landmarks",icon: HandPlatter},
+        { id: "20", title: "Adventure",  image: "https://i.pinimg.com/736x/58/44/2d/58442d48aff42c89f0634932459da311.jpg" ,subtitle: "Enjoy luxury style landmarks",icon: HandPlatter},
+
+
+        { id: "2", title: "Relaxation", image: "https://i.pinimg.com/736x/bb/91/fd/bb91fdd139fdb20e73a70f8f7d2002d1.jpg" ,subtitle: "Beaches, spa, slow mornings",icon: Umbrella},
+        { id: "2", title: "Relaxation", image: "https://i.pinimg.com/736x/4d/59/57/4d595742414c568cdd63bce3619d8415.jpg.jpg" ,subtitle: "Beaches, spa, slow mornings",icon: Umbrella},
+        { id: "2", title: "Relaxation", image: "https://i.pinimg.com/736x/7d/4b/f9/7d4bf9a54fe4caf154e0af70c48128d0.jpg" ,subtitle: "Beaches, spa, slow mornings",icon: Umbrella},
+        { id: "2", title: "Relaxation", image: "https://i.pinimg.com/736x/7f/22/4f/7f224f8d1759a5819c1636295be3a923.jpg" ,subtitle: "Beaches, spa, slow mornings",icon: Umbrella},
+        { id: "2", title: "Relaxation", image: "https://i.pinimg.com/736x/ec/8c/a3/ec8ca3475a19fc7553ebfaed0395c03e.jpg" ,subtitle: "Beaches, spa, slow mornings",icon: Umbrella},
+
+
+        { id: "3", title: "Culture", image: "https://i.pinimg.com/736x/fd/aa/75/fdaa75f0854b00753f0bd8a722676836.jpg",subtitle: "Museums, festivals",icon: Landmark, },
+        { id: "21", title: "Culture", image: "https://i.pinimg.com/736x/ee/37/95/ee3795534b534038718a4c8f6efa5c92.jpg",subtitle: "Museums, festivals",icon: Landmark, },
+        { id: "22", title: "Culture", image: "https://i.pinimg.com/1200x/a0/ac/e1/a0ace1a02fa9dd4c1d6ae6e24ea3cec7.jpg",subtitle: "Museums, festivals",icon: Landmark, },
+        { id: "23", title: "Culture", image: "https://i.pinimg.com/736x/9d/ae/29/9dae29312791dc769d0f25159bde4db1.jpg",subtitle: "Museums, festivals",icon: Landmark, },
+        { id: "24", title: "Culture", image: "https://i.pinimg.com/1200x/c2/2a/b8/c22ab8d199ebe9fb765f030096e9d3f2.jpg",subtitle: "Museums, festivals",icon: Landmark, },
+        { id: "25", title: "Culture", image: "https://i.pinimg.com/1200x/89/55/b1/8955b1a0fadd27feef634ca37bed9beb.jpg",subtitle: "Museums, festivals",icon: Landmark, },
+        { id: "26", title: "Culture", image: "https://i.pinimg.com/1200x/b8/61/2f/b8612f53611d1a6c7e897f5b5118caba.jpg",subtitle: "Museums, festivals",icon: Landmark, },
+        { id: "27", title: "Culture", image: "https://i.pinimg.com/1200x/e7/ff/21/e7ff21a41465133bc5ba9e40699b84c3.jpg",subtitle: "Museums, festivals",icon: Landmark, },
+        { id: "28", title: "Culture", image: "https://i.pinimg.com/736x/43/23/1f/43231fce747cf60db049c91bd3432898.jpg",subtitle: "Museums, festivals",icon: Landmark, },
+        { id: "29", title: "Culture", image: "https://i.pinimg.com/736x/91/14/7c/91147c2fc0b21f25543b56ff7573d942.jpg",subtitle: "Museums, festivals",icon: Landmark, },
+
+
+
+        { id: "4",  title: "Sightseeing", image:  "https://i.pinimg.com/736x/75/d8/39/75d8398a1233c844354c2911a3b6bf79.jpg",subtitle: "Explore famous landmarks",icon: Binoculars , },
+        { id: "30",  title: "Sightseeing", image:  "https://i.pinimg.com/736x/75/d8/39/75d8398a1233c844354c2911a3b6bf79.jpg",subtitle: "Explore famous landmarks",icon: Binoculars , },
+        { id: "31",  title: "Sightseeing", image:  "https://i.pinimg.com/736x/2d/5e/8e/2d5e8e79797df03d6195d0ec323bea72.jpg",subtitle: "Explore famous landmarks",icon: Binoculars , },
+        { id: "32",  title: "Sightseeing", image:  "https://i.pinimg.com/736x/fc/91/61/fc91610dace7f7521aca967567f9c305.jpg",subtitle: "Explore famous landmarks",icon: Binoculars , },
+        { id: "33",  title: "Sightseeing", image:  "https://i.pinimg.com/1200x/a1/da/76/a1da76005bcbc624d0ca3972e4b595ea.jpg",subtitle: "Explore famous landmarks",icon: Binoculars , },
+        { id: "34",  title: "Sightseeing", image:  "https://i.pinimg.com/1200x/63/64/74/636474eee93213b1e2486b81f98e2b79.jpg",subtitle: "Explore famous landmarks",icon: Binoculars , },
+
+
+        { id: "35", title: "Dining", image:  "https://i.pinimg.com/1200x/0b/68/f2/0b68f23e83104e3057b8d79bc6ba7d7c.jpg",subtitle: " Discover food taste experiences",icon: HandPlatter },
+        { id: "36", title: "Dining", image:  "https://i.pinimg.com/736x/86/bc/12/86bc126737ec9b149473ad1ef17c6875.jpg",subtitle: " Discover food taste experiences",icon: HandPlatter },
+        { id: "37", title: "Dining", image:  "https://i.pinimg.com/736x/b2/8a/ce/b28ace4fb98ecbe35e1d8e323b9b5355.jpg",subtitle: " Discover food taste experiences",icon: HandPlatter },
+        { id: "38", title: "Dining", image:  "https://i.pinimg.com/1200x/ec/80/3d/ec803df55fd58f816d359ca96592e174.jpg",subtitle: " Discover food taste experiences",icon: HandPlatter },
+
+
+        { id: "46", title: "Outdoor", image: "https://i.pinimg.com/736x/03/e8/4c/03e84cd919058020dc3f12bfcea198c9.jpg" ,subtitle: "Enjoy nature with camping",icon: TentTree},
+        { id: "47", title: "Outdoor", image: "https://i.pinimg.com/736x/bd/52/4a/bd524a79f027e6181b994ffb50903e49.jpg" ,subtitle: "Enjoy nature with camping",icon: TentTree},
+        { id: "48", title: "Outdoor", image: "https://i.pinimg.com/736x/36/89/74/368974b470f426b4d57d70981f85ad73.jpg" ,subtitle: "Enjoy nature with camping",icon: TentTree},
+
+
+
+        { id: "58", title: "Solo",    image:  "https://i.pinimg.com/1200x/37/05/73/3705737db12bde76985204ec6fa83ee1.jpg",subtitle: "Explore famous Solo",icon: Tent , },
+        { id: "59", title: "Solo",    image:  "https://i.pinimg.com/736x/11/e1/92/11e19268abe6c847d332dec953253263.jpg",subtitle: "Explore famous Solo",icon: Tent , },
+        { id: "50", title: "Solo",    image:  "https://i.pinimg.com/736x/7a/2d/6f/7a2d6f74c8223a273fe68e365a872d80.jpg",subtitle: "Explore famous Solo",icon: Tent , },
+        { id: "49", title: "Solo",    image:  "https://i.pinimg.com/736x/a1/3d/0a/a13d0ae81832535fe6b2cd2cc91358bd.jpg",subtitle: "Explore famous Solo",icon: Tent , },
+
+
+
+
+        { id: "8", title: "Family",  image:  "https://i.pinimg.com/1200x/b2/f3/45/b2f3459b5be00a47f3a52e9c01a212a6.jpg",subtitle: " Discover family destination",icon: HousePlus },
+        { id: "8", title: "Family",  image:  "https://i.pinimg.com/1200x/b3/29/85/b32985f2da7134730d17b86c78c06da5.jpg",subtitle: " Discover family destination",icon: HousePlus },
+        { id: "8", title: "Family",  image:  "https://i.pinimg.com/736x/7c/4b/04/7c4b04bfa26176fa2ee0828df4d9f9a3.jpg",subtitle: " Discover family destination",icon: HousePlus },
+        { id: "8", title: "Family",  image:  "https://i.pinimg.com/1200x/b2/f3/45/b2f3459b5be00a47f3a52e9c01a212a6.jpg",subtitle: " Discover family destination",icon: HousePlus },
+
+
+
+        { id: "10", title: "Luxury",  image: "https://i.pinimg.com/1200x/ec/a7/c4/eca7c4931e71dd9edd89876b36e10397.jpg" ,subtitle: "Enjoy luxury style landmarks",icon: HandPlatter},
+        { id: "11", title: "Luxury",  image: "https://i.pinimg.com/1200x/ec/a7/c4/eca7c4931e71dd9edd89876b36e10397.jpg" ,subtitle: "Enjoy luxury style landmarks",icon: HandPlatter},
+        { id: "12", title: "Luxury",  image: "https://i.pinimg.com/1200x/ec/a7/c4/eca7c4931e71dd9edd89876b36e10397.jpg" ,subtitle: "Enjoy luxury style landmarks",icon: HandPlatter},
+        { id: "13", title: "Luxury",  image: "https://i.pinimg.com/1200x/ec/a7/c4/eca7c4931e71dd9edd89876b36e10397.jpg" ,subtitle: "Enjoy luxury style landmarks",icon: HandPlatter},
+        { id: "14", title: "Luxury",  image: "https://i.pinimg.com/1200x/ec/a7/c4/eca7c4931e71dd9edd89876b36e10397.jpg" ,subtitle: "Enjoy luxury style landmarks",icon: HandPlatter},
+        { id: "15", title: "Luxury",  image: "https://i.pinimg.com/1200x/ec/a7/c4/eca7c4931e71dd9edd89876b36e10397.jpg" ,subtitle: "Enjoy luxury style landmarks",icon: HandPlatter},
+
+   ];
